b4a8b203c37bdedff5b08a72650c81740fd70276
Adambizien/blog-symfony <bizienadam@outlook.com>
2023-11-10 20:47:38 +0100

    slq code

--- Database.sql
+++ Database.sql
@@ -4,10 +4,53 @@ USE BoutiqueSC;
 
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    lastname VARCHAR(50),
-    email VARCHAR(100),
+    name VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     address VARCHAR(255),
-    password VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
     role INT
+);
+
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    image LONGBLOB NOT NULL,
+    first BOOLEAN NOT NULL,
+    date DATETIME NOT NULL,
+    online BOOLEAN NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+INSERT INTO category (id, name)
+VALUES (1, 'Véhicule terrestre'), (2, 'Véhicule spatial');
+
+
+
+
+CREATE TABLE statusCommande (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO statusCommande (id, name)
+VALUES (1, 'En attente de paiement'), (2, 'En attente de livraison'), (3, 'En cours de livraison'), (4, 'Livré');
+
+CREATE TABLE commande (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    status_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (status_id) REFERENCES statusCommande(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
